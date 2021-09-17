b6d32e975040437dfa5b1bac87410645aed0ac3e
ctdax <colbythomps@gmail.com>
2021-09-17 18:07:51 -0400

    Update VertexImageCreator.py
    Tracks are now grayscale depending on primary vertex displacement error.
    The magnitude of the triangle base now depends on track pt.
    Generalized MaxVar function.
    Removed unnecessary code.
    Added comments

--- VertexImageCreator.py
+++ VertexImageCreator.py
@@ -1,31 +1,39 @@
 import math as m
 import uproot
-from matplotlib import pyplot as plt
-import numpy as np
 from PIL import Image, ImageDraw
 
-def drawLine(shape, img):
-    img.line(xy=shape, fill='black', width=1)
+def drawLine(shape, img, tkdxy_err, max_dxy_err): #Draws a line whose magnitude is the magnitude of px and py
+    #Calculate the greyscale of the trinagle based on the tk primary dxy error
+    grayscale = int(200 * tkdxy_err/max_dxy_err)
+
+    #Draws the line
+    img.line(xy=shape, fill=grayscale, width=1)
 
 
-def drawTriangle(lineshape, img):
+def drawTriangle(lineshape, img, imagecenter, tkdxy_err, max_dxy_err, tk_pt, max_tk_pt): #Draw isoceles triangle where magnitude of the track is the height
     line_magnitude = m.sqrt((lineshape[1][0]-lineshape[0][0])**2 + (lineshape[1][1]-lineshape[0][1])**2)
-    base_magnitude = line_magnitude/4
+
+    #Calculate magnitude of the base based on the pt of the track
+    base_magnitude = (line_magnitude/3) * m.log(tk_pt)/m.log(max_tk_pt)
+
+    #Calculate corners of the triangle
     xcorner, ycorner = base_magnitude/m.sqrt(2), base_magnitude/m.sqrt(2)
-    center = (lineshape[0][0], lineshape[0][1])
-    if lineshape[1][0] < center[0] and lineshape[1][1] < center[0]:
+    initial_points = (lineshape[0][0], lineshape[0][1])
+    if (lineshape[1][0] < imagecenter and lineshape[1][1] < imagecenter) or (lineshape[1][0] > imagecenter and lineshape[1][1] > imagecenter):
         corner1 = (lineshape[1][0]+xcorner, lineshape[1][1]-ycorner)
         corner2 = (lineshape[1][0]-xcorner, lineshape[1][1]+ycorner)
-    elif lineshape[1][0] > center[0] and lineshape[1][1] > center[0]:
-        corner2 = (lineshape[1][0] + xcorner, lineshape[1][1] - ycorner)
-        corner1 = (lineshape[1][0] - xcorner, lineshape[1][1] + ycorner)
     else:
         corner1 = (lineshape[1][0] - xcorner, lineshape[1][1] - ycorner)
         corner2 = (lineshape[1][0] + xcorner, lineshape[1][1] + ycorner)
-    img.polygon(xy=[center, corner1, corner2], fill=None, outline=None)
+
+    #Calculate the greyscale of the trinagle based on the tk primary dxy error
+    grayscale = int(200 * tkdxy_err/max_dxy_err)
+
+    #Draw triangle
+    img.polygon(xy=[initial_points, corner1, corner2], fill=None, outline=grayscale)
 
 
-def calculateAngle(x, y):
+def calculateAngle(x, y): #Returns the 360 degree angle for a given 2D vector
     try:
         theta = m.degrees(m.atan(y/x))
         abstheta = abs(theta)
@@ -51,29 +59,26 @@ def calculateAngle(x, y):
     return abstheta
 
 
-def GetMaxP(px, py, pz):
-    maxpx = []
-    maxpy = []
-    maxpz = []
-    for a in px:
+def GetMaxVar(var): #Returns the maximum value of a particular variable
+    maxL = []
+    for a in var:
         for b in a:
-            maxpx.append(max(b))
-    for a in py:
-        for b in a:
-            maxpy.append(max(b))
-    for a in pz:
-        for b in a:
-            maxpz.append(max(b))
+            maxL.append(max(b))
 
-    return max(maxpx), max(maxpy), max(maxpz)
+    return max(maxL)
 
 
-def calculateXYImage(px, py, pz, maxpx, maxpy, maxpz, tk2_dx, tk2_dy, imagecenter, scale_multiplicity_constant): #Calculate the endpoints of the track lines for the images in x y space
-    xilist = []
-    yilist = []
-    xflist = []
-    yflist = []
-    cx, cy, cz = scale_multiplicity_constant*imagecenter/m.log(maxpx), scale_multiplicity_constant*imagecenter/m.log(maxpy), scale_multiplicity_constant*imagecenter/m.log(maxpz)
+def calculateXYImage(px, py, maxpx, maxpy, tk2_dx, tk2_dy, imagecenter, scale_multiplicity_constant): #Calculate the endpoints of the track lines for the images in x y space
+    #Initialize the lists
+    x_initial_list = []
+    y_initial_list = []
+    x_final_list = []
+    y_final_list = []
+
+    #Create track magnitude constant
+    cx, cy = scale_multiplicity_constant*imagecenter/m.log(maxpx), scale_multiplicity_constant*imagecenter/m.log(maxpy)
+
+    #Resize the px and py components of the track in accordance to the constant and the log of px/py
     for i in range(len(px)):
         try:
             clogx = cx*m.log(px[i])
@@ -84,10 +89,6 @@ def calculateXYImage(px, py, pz, maxpx, maxpy, maxpz, tk2_dx, tk2_dy, imagecente
         except ValueError:
             clogy = -1*cy*m.log(abs(py[i]))
         try:
-            clogz = cz*m.log(pz[i])
-        except ValueError:
-            clogz = -1*cz*m.log(abs(pz[i]))
-        try:
             clogdx = m.log(tk2_dx[i])
         except ValueError:
             clogdx = -1*m.log(abs(tk2_dx[i]))
@@ -95,23 +96,24 @@ def calculateXYImage(px, py, pz, maxpx, maxpy, maxpz, tk2_dx, tk2_dy, imagecente
             clogdy = m.log(tk2_dy[i])
         except ValueError:
             clogdy = -1*m.log(abs(tk2_dy[i]))
-        #xlist.append(round(imagecenter+clogx+clogdx+(clogz/m.sqrt(2))))
-        #ylist.append(round(imagecenter+clogy+clogdy+(clogz/m.sqrt(2))))
-        xilist.append(round(imagecenter+clogdx))
-        yilist.append(round(imagecenter+clogdy))
-        xflist.append(round(imagecenter+clogx))
-        yflist.append(round(imagecenter+clogy))
+        x_initial_list.append(round(imagecenter+clogdx))
+        y_initial_list.append(round(imagecenter+clogdy))
+        x_final_list.append(round(imagecenter+clogx))
+        y_final_list.append(round(imagecenter+clogy))
 
-    return xilist, yilist, xflist, yflist
+    return x_initial_list, y_initial_list, x_final_list, y_final_list
 
 
-def calculateSecondaryDXY(tk_px, tk_py, tk_dxy, vtx_x, vtx_y):
+def calculateSecondaryDXY(tk_px, tk_py, tk_dxy, vtx_x, vtx_y): #Calculates the track dxy with respect to the secondary vertex
+    #Initialize list and variables
     vtx_mag = m.sqrt(vtx_x**2 + vtx_y**2)
     vtx_theta = calculateAngle(vtx_x, vtx_y)
     tkp_theta = []
     tk2_dxy = []
     tk2_dx = []
     tk2_dy = []
+
+    #Calculate the secondary dxy
     for i in range(len(tk_px)):
         tkp_theta.append(calculateAngle(tk_px[i], tk_py[i]))
         tk2_dxy.append(tk_dxy[i] - m.sin(m.radians(tkp_theta[i]-vtx_theta))*vtx_mag)
@@ -121,42 +123,28 @@ def calculateSecondaryDXY(tk_px, tk_py, tk_dxy, vtx_x, vtx_y):
             omega = tkp_theta[i] - 90
         tk2_dx.append(tk2_dxy[i]*m.cos(m.radians(omega)))
         tk2_dy.append(tk2_dxy[i]*m.sin(m.radians(omega)))
-    print(tk2_dxy, '\n', tk2_dx, '\n', tk2_dy)
 
     return tk2_dx, tk2_dy
 
 
-def RotateComponents(xilist, yilist, xflist, yflist, vtx_theta):
-    xflist_rotated = []
-    yflist_rotated = []
-    for i in range(len(xilist)):
-        deltax = xflist[i]-xilist[i]
-        deltay = yflist[i]-yilist[i]
-        deltamag = m.sqrt(deltax**2 + deltay**2)
-        tk_theta = calculateAngle(deltax, deltay)
-        rotation_angle = 180 + vtx_theta + tk_theta
-        while rotation_angle >= 360:
-            rotation_angle -= 360
+def RotateComponents(x_initial_list, y_initial_list, x_final_list, y_final_list, vtx_theta): #Rotates the tracks so that the up direction points away from the primary vertex
+    #Initalize the lists
+    x_final_list_rotated = []
+    y_final_list_rotated = []
+    x_final_list_rotated_fixed = []
+    y_final_list_rotated_fixed = []
 
-        if rotation_angle >= 0 and rotation_angle <= 90:
-            xflist_rotated.append(deltamag*m.cos(m.radians(rotation_angle)))
-            yflist_rotated.append(deltamag*m.sin(m.radians(rotation_angle)))
-        elif rotation_angle > 90  and rotation_angle <= 180:
-            rotation_angle_fixed = rotation_angle - 90
-            xflist_rotated.append(-1 * deltamag * m.cos(m.radians(rotation_angle_fixed)))
-            yflist_rotated.append(deltamag * m.sin(m.radians(rotation_angle_fixed)))
-        elif rotation_angle > 180 and rotation_angle <= 270:
-            rotation_angle_fixed = rotation_angle - 180
-            xflist_rotated.append(-1 * deltamag*m.cos(m.radians(rotation_angle_fixed)))
-            yflist_rotated.append(-1 * deltamag*m.sin(m.radians(rotation_angle_fixed)))
-        else:
-            rotation_angle_fixed = rotation_angle - 270
-            xflist_rotated.append(deltamag * m.cos(m.radians(rotation_angle_fixed)))
-            yflist_rotated.append(-1 * deltamag * m.sin(m.radians(rotation_angle_fixed)))
+    #Rotate the x and y components
+    for i in range(len(x_initial_list)):
+        deltax = x_final_list[i]-x_initial_list[i]
+        deltay = y_final_list[i]-y_initial_list[i]
+        rotation_angle = vtx_theta + 180
+        x_final_list_rotated.append(deltax*m.cos(m.radians(rotation_angle)) - deltay*m.sin(m.radians(rotation_angle)))
+        y_final_list_rotated.append(deltax * m.sin(m.radians(rotation_angle)) + deltay * m.cos(m.radians(rotation_angle)))
+        x_final_list_rotated_fixed.append(x_initial_list[i] + x_final_list_rotated[i])
+        y_final_list_rotated_fixed.append(y_initial_list[i] + y_final_list_rotated[i])
 
-    return xflist_rotated, yflist_rotated
-
-
+    return x_final_list_rotated_fixed, y_final_list_rotated_fixed
 
 
 #Import ROOT files
@@ -171,17 +159,23 @@ print(stree.keys())
 #Obtain momentum data
 px = stree['vtx_tk_px'].array()
 py = stree['vtx_tk_py'].array()
-pz = stree['vtx_tk_pz'].array()
+pt = stree['vtx_tk_pt'].array()
 dxy = stree['vtx_tk_dxy'].array()
+dxy_err = stree['vtx_tk_dxyerr'].array()
 vtx_x = stree['vtx_x'].array()
 vtx_y = stree['vtx_y'].array()
-maxpx, maxpy, maxpz = GetMaxP(px, py, pz)
+
+#Obtain max data
+maxpx, maxpy, maxpt, max_dxy_err = GetMaxVar(px), GetMaxVar(py), GetMaxVar(pt), GetMaxVar(dxy_err)
+
+#Obtain first vertex data
 px0 = px[0][0]
 py0 = py[0][0]
-pz0 = pz[0][0]
+pt0 = pt[0][0]
 dxy0 = dxy[0][0]
 vtx_x0 = vtx_x[0][0]
 vtx_y0 = vtx_y[0][0]
+dxy_err0 = dxy_err[0][0]
 
 #Calculate secondary dxy
 tk2_dx, tk2_dy = calculateSecondaryDXY(px0, py0, dxy0, vtx_x0, vtx_y0)
@@ -189,26 +183,18 @@ tk2_dx, tk2_dy = calculateSecondaryDXY(px0, py0, dxy0, vtx_x0, vtx_y0)
 #Create image
 w, h = 224, 224
 center = int(w/2)
-xilist, yilist, xflist, yflist = calculateXYImage(px0, py0, pz0, maxpx, maxpy, maxpz, tk2_dx, tk2_dy, center, 1)
-img = Image.new(mode='1', size=(w, h), color=1)
+x_initial_list, y_initial_list, x_final_list, y_final_list = calculateXYImage(px0, py0, maxpx, maxpy, tk2_dx, tk2_dy, center, 2/3)
+img = Image.new(mode='L', size=(w, h), color=255)
 img1 = ImageDraw.Draw(img)
 
 #Rotate components so that up direction points away from primary vertex
 vtx_theta = calculateAngle(vtx_x0, vtx_y0)
-xflist_rotated, yflist_rotated = RotateComponents(xilist, yilist, xflist, yflist, vtx_theta)
-xflist_rotated_fixed = []
-yflist_rotated_fixed = []
-for i in range(len(xilist)):
-    xflist_rotated_fixed.append(xilist[i] + xflist_rotated[i])
-    yflist_rotated_fixed.append(yilist[i] + yflist_rotated[i])
-
-print('xilist: ', xilist, '\nxflist: ', xflist, '\nxflist_rotated: ', xflist_rotated_fixed,
-      '\nyilist: ', yilist, '\nyflist: ', yflist, '\nyflist_rotated: ', yflist_rotated_fixed)
+x_final_list_rotated, y_final_list_rotated = RotateComponents(x_initial_list, y_initial_list, x_final_list, y_final_list, vtx_theta)
 
 #Draw Triangles
-for i in range(len(xflist)):
-    shape = [(xilist[i], yilist[i]), (xflist[i], yflist[i])]
-    #shape = [(xilist[i], yilist[i]), (xflist_rotated_fixed[i], yflist_rotated_fixed[i])]
-    #drawTriangle(shape, img1)
-    drawLine(shape, img1)
+for i in range(len(x_final_list)):
+    #shape = [(x_initial_list[i], y_initial_list[i]), (x_final_list[i], y_final_list[i])]
+    shape = [(x_initial_list[i], y_initial_list[i]), (x_final_list_rotated[i], y_final_list_rotated[i])]
+    drawTriangle(shape, img1, center, dxy_err0[i], max_dxy_err, pt0[i], maxpt)
+    #drawLine(shape, img1, dxy_err0[i], max_dxy_err)
 img.show()
